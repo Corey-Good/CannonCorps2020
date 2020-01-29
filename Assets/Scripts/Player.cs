5fd7bb14b4943e6ac0d9f1829849aa9a874e11dd
MediocrePick <55897070+MediocrePick@users.noreply.github.com>
2020-01-29 16:13:20 -0600

    Created the Player and Tank scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/************************************************************************/
+/* Author: Eddie Habal */
+/* Date Created: 1/29/2020 */
+/* Last Modified Date: */
+/* Modified By: */
+/************************************************************************/
+
+public class Player : MonoBehaviour
+{
+    private int playerID { get; set; }
+    private string playerName { get; set; }
+    private int killsCurrent { get; set; }
+    private int killsAlltime { get; set; }
+    private int killsInARow { get; set; }
+    private int scoreCurrent { get; set; }
+    private int scoreAlltime { get; set; }
+    private int deathsCurrent { get; set; }
+    private int deathsAlltime { get; set; }
+    private int deathsInARow { get; set; }
+
+
+
+}
---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/************************************************************************/
+/* Author: Eddie Habal */
+/* Date Created: 1/29/2020 */
+/* Last Modified Date: */
+/* Modified By: */
+/************************************************************************/
+
+public class Tank : MonoBehaviour
+{
+    private int   healthMax     { get; set; }
+    private int   healthCurrent { get; set; }
+    private int   healthRegen   { get; set; }
+    private float speedRotation { get; set; }
+    private float speedMovement { get; set; }
+    private float bulletSpeed   { get; set; }
+    private int   bulletDamage  { get; set; }
+    private float bulletReload  { get; set; }
+
+    private GameObject tankModel { get; set; }
+    private GameObject tankProjectile { get; set; }
+
+    private Color tankColor { get; set; }
+
+    public Tank (GameObject tankModelChosen, Color tankColorChosen)
+    {
+        switch(tankModelChosen.name)
+        {
+            case "cartoonTank":
+                tankColor     = tankColorChosen;
+                healthMax     = 100;
+                healthCurrent = 100;
+                healthRegen   = 5;
+                speedRotation = 5;
+                speedMovement = 50;
+                bulletSpeed   = 50;
+                bulletDamage  = 20;
+                bulletReload  = 10;
+                break;
+
+            case "futureTank":
+                tankColor     = tankColorChosen;
+                healthMax     = 100;
+                healthCurrent = 100;
+                healthRegen   = 5;
+                speedRotation = 5;
+                speedMovement = 50;
+                bulletSpeed   = 50;
+                bulletDamage  = 20;
+                bulletReload  = 10;
+                break;
+
+            case "catapult":
+                tankColor     = tankColorChosen;
+                healthMax     = 100;
+                healthCurrent = 100;
+                healthRegen   = 5;
+                speedRotation = 5;
+                speedMovement = 50;
+                bulletSpeed   = 50;
+                bulletDamage  = 20;
+                bulletReload  = 10;
+                break;
+
+            case "boxTank":
+                tankColor     = tankColorChosen;
+                healthMax     = 100;
+                healthCurrent = 100;
+                healthRegen   = 5;
+                speedRotation = 5;
+                speedMovement = 50;
+                bulletSpeed   = 50;
+                bulletDamage  = 20;
+                bulletReload  = 10;
+                break;
+               
+        }
+    }
+
+}
