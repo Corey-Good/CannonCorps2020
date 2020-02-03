788d39b464273c9abe32018f28658f5a2254b11f
Corey-Good <55670356+Corey-Good@users.noreply.github.com>
2020-02-03 13:58:45 -0600

    Finished merging changes

---
+++ Assets/Scripts/ManageScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScene : MonoBehaviour
+{
+
+    private static ManageScene manageSceneInstance;
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        if (manageSceneInstance == null)
+        {
+            manageSceneInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SceneManager.LoadScene(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(1);
+        }       
+
+    }
+
+}
