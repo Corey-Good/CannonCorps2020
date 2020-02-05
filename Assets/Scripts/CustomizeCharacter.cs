d1aad02fe8aaa8708547383b89f8ca9f2996779b
Corey-Good <coreygood6@gmail.com>
2020-02-04 20:16:33 -0600

    Added and tested some models

---
+++ Assets/Scripts/CustomizeCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizeCharacter : MonoBehaviour
+{
+    private Tank tankInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tankInstance = GetTankInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //tankInstance.CreateTank("boxTank", Color.red);
+
+        //tankInstance.damageTaken(10.0f);
+    }
+    public Tank GetTankInstance()
+    {
+        return GameObject.FindGameObjectWithTag("TankClass").GetComponent<Tank>();
+    }
+}
