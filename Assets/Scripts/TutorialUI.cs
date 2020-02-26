78746c326ce1b0c66cedf9bf6cff0929ce31b64b
JabenCalas <55771813+JabenCalas@users.noreply.github.com>
2020-02-26 15:53:55 -0600

    Updates

--- Assets/Scripts/TutorialUI.cs
+++ Assets/Scripts/TutorialUI.cs
@@ -13,21 +13,15 @@ using TMPro;
 
 public class TutorialUI : MonoBehaviour
 {
-    public List<GameObject> tutorialText = new List<GameObject>();
+    public  KeyCode         nextKey;
+    public  KeyCode         prevKey;
 
-    public KeyCode nextKey;
-    public KeyCode prevKey;
-    private int   stepCounter = 0;
+    private int             stepCounter   = 0;
+    private float           promptDelay   = 2.0f;
 
-    private float promptDelay = 2.0f;
-
-    public TextMeshProUGUI movementKeys;
-
-    public TextMeshProUGUI mainText;
-    public TextMeshProUGUI subText;
-
-    public GameObject moveHere;
-    public GameObject lookHere;
+    public  TextMeshProUGUI headingText;
+    public  TextMeshProUGUI subtitleText;
+    public  TextMeshProUGUI actionText;
 
     [TextArea(2, 10)]
     public string[] sentences;
@@ -55,53 +49,52 @@ public class TutorialUI : MonoBehaviour
         switch (stepCounter)
         {
             case 0:
-                mainText.text = sentences[0]; // Welcome to the training camp!
-                subText.text  = sentences[1]; // Here you will learn the basic skills required in battle.
-                Invoke("PressToContinue", promptDelay);
+                headingText.text = sentences[0]; // Welcome to the training camp!
+                subtitleText.text  = sentences[1]; // Here you will learn the basic skills required in battle.
+                Invoke("PressKeyToContinue", promptDelay);
                 break;
 
             case 1:
-                mainText.text = sentences[2]; // Use your mouse to control the camera.
-                subText.text  = sentences[3]; // The turret follows the camera as you move it.
-                Invoke("PressToContinue", promptDelay);
+                headingText.text = sentences[2]; // Use your mouse to control the camera.
+                subtitleText.text  = sentences[3]; // The turret follows the camera as you move it.
+                Invoke("PressKeyToContinue", promptDelay);
                 break;
 
             case 2:
-                mainText.text = GetKeys(); // Use the W, A, S, and D keys to control your vehicle.
-                Invoke("PressToContinue", promptDelay);
+                headingText.text = GetKeys(); // Use the W, A, S, and D keys to control your vehicle.
+                Invoke("PressKeyToContinue", promptDelay);
                 break;
 
             case 3:
-                mainText.text = sentences[6]; // Move to the designated location.
-                Invoke("PressToContinue", promptDelay);
+                headingText.text = sentences[6]; // Move to the designated location.
+                Invoke("PressKeyToContinue", promptDelay);
                 break;
 
             case 4:
-                mainText.text = sentences[8]; // CONGRATULATIONS!
-                subText.text  = sentences[9]; // You have successfully completed the tutorial.
-                Invoke("PressToContinue", promptDelay);
+                headingText.text = sentences[8]; // CONGRATULATIONS!
+                subtitleText.text  = sentences[9]; // You have successfully completed the tutorial.
+                Invoke("PressKeyToContinue", promptDelay);
                 break;
         }
     }
+    #endregion
     string GetKeys()
     {
-        string keybindings;
-
-        keybindings = string.Format("Use the {0}, {1}, {2}, and {3} keys to control your vehicle.", 
+        string keybindings = string.Format("Use the {0}, {1}, {2}, and {3} keys to control your vehicle.",
             KeyBindings.forwardKey, KeyBindings.leftKey, KeyBindings.backwardKey, KeyBindings.rightKey);
 
         return keybindings;
     }
-    #endregion
 
-    void OnCollisionEnter(Collision collisionInfo)
+    void PressKeyToContinue()
     {
-        if(collisionInfo.collider.tag == "TutorialUI")
+        if (Input.anyKeyDown)
         {
-            if(collisionInfo.collider.gameObject == moveHere)
-            {
-
-            }
+            actionText.text = sentences[13]; // Completed!
+        }
+        else
+        {
+            actionText.text = sentences[12]; // Press any key to continue.
         }
     }
 }
