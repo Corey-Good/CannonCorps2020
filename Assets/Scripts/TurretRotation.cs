f8c845fd79d9b6e5b45ea0e86625bf96aeec11ca
M3GAalien <55897035+M3GAalien@users.noreply.github.com>
2020-02-26 15:33:17 -0600

    Added proper formating
    Completed the header info
    
    clarified the use for the update() function
    
    reformated turretObject function to fit within the screen

--- Assets/Scripts/TurretRotation.cs
+++ Assets/Scripts/TurretRotation.cs
@@ -1,8 +1,8 @@
 ﻿/************************************************************************/
 /* Author:             Jaben Calas                                      */
 /* Date Created:       02/14/2020                                       */
-/* Last Modified Date:                                                  */
-/* Modified By:                                                         */
+/* Last Modified Date: 02/26/2020                                       */
+/* Modified By:        M. Agamalian                                     */
 /************************************************************************/
 
 using UnityEngine;
@@ -11,11 +11,13 @@ public class TurretRotation : MonoBehaviour
 {
     public GameObject turretObject;
 
+    // Every frame, sync the turrent rotation to the movement of the mouse
     private void Update()
     {
         if (!PauseMenuAnimations.GameIsPaused)
         {
-            turretObject.transform.Rotate(0f, 0, Input.GetAxis("Mouse X") * 300 * Time.deltaTime);
+            turretObject.transform.Rotate
+            (0f, 0, Input.GetAxis("Mouse X") * 300 * Time.deltaTime);
         }
     }
 }
