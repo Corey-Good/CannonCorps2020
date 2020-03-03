d3f163f2a0bec0153a69c4018e99bdcbda1fa924
JabenCalas <55771813+JabenCalas@users.noreply.github.com>
2020-03-03 15:44:50 -0600

    Working on ToolTips
    Background already implemented.

---
+++ Assets/Scripts/MainMenuToolTips.cs
@@ -0,0 +1,22 @@
+﻿/************************************************************************/
+/* Author:             Jaben Calas                                      */
+/* Date Created:       3/03/2020                                        */
+/* Last Modified Date: 3/03/2020                                        */
+/* Modified By:        J. Calas                                         */
+/************************************************************************/
+using UnityEngine;
+
+public class MainMenuToolTips : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
