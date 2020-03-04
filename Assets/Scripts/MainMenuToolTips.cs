d89fca376b0171aeed8cf82c108d8c6e8fea079c
JabenCalas <55771813+JabenCalas@users.noreply.github.com>
2020-03-03 20:17:24 -0600

    Completed ToolTips

--- Assets/Scripts/MainMenuToolTips.cs
+++ Assets/Scripts/MainMenuToolTips.cs
@@ -5,9 +5,11 @@
 /* Modified By:        J. Calas                                         */
 /************************************************************************/
 using UnityEngine;
-
+using TMPro;
 public class MainMenuToolTips : MonoBehaviour
 {
+    public TextMeshProUGUI ToolTipsText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,27 @@ public class MainMenuToolTips : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    public void FFATips()
+    {
+        ToolTipsText.text = "Try to get as many points as possible by eliminating opponents!";
+    }
+    public void SMTips()
+    {
+       ToolTipsText.text =     "All players start off as minnows, with 1 player randomly becoming a shark." +
+                           "\n\nThe shark will try to overpower as many minnows as possible, while minnows try to survive " +
+                               "for as long as possible!";
+    }
+    public void TBTips()
+    {
+       ToolTipsText.text =     "Defeat players on the enemy team to earn points for your team. " +
+                           "\n\nThe team that reaches the score limit, or has more points when time expires wins!";
+    }
+    public void TTTips()
+    {
+        ToolTipsText.text = "Click here to learn the basics!";
     }
 }
