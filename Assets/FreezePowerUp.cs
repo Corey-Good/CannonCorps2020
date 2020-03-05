6f915bd4617f9348f8658b8e10b9ac2295f7efd2
MediocrePick <55897070+MediocrePick@users.noreply.github.com>
2020-03-05 14:52:45 -0600

    freeze powerup works

---
+++ Assets/FreezePowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezePowerUp : PowerUp
+{
+    public float rotateMultiplier = 0f;
+    public float speedMultiplier = 0f;
+
+    protected override void PowerUpPayload()          // Checklist item 1
+    {
+        base.PowerUpPayload();
+        playerBrain.SetSpeedBoostOn(speedMultiplier, rotateMultiplier);
+        StartCoroutine(FreezeTimer());
+    }
+
+    IEnumerator FreezeTimer()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+        PowerUpHasExpired();
+    }
+
+    protected override void PowerUpHasExpired()       // Checklist item 2
+    {
+        playerBrain.SetSpeedBoostOff();
+        base.PowerUpHasExpired();
+    }
+}
