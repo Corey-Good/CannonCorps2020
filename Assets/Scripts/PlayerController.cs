2485b6cad2c49f0f9f31221f3c4a83e40d9585b2
Corey-Good <55670356+Corey-Good@users.noreply.github.com>
2020-03-07 09:35:44 -0600

    WOrked on animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -97,7 +97,7 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
             if (fireAnimation != null)
             {
                 Debug.Log("Firing the Catapult!!!");
-                fireAnimation.SetTrigger("LaunchCatapult");
+                fireAnimation.SetTrigger("Fire");
             }
         }
 
