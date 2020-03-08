9882eca25d04d8458b0daf02042b3e8e9894b80e
JabenCalas <55771813+JabenCalas@users.noreply.github.com>
2020-03-08 18:08:25 -0500

    Progress made toward TutorialMode

--- Assets/Scripts/TutorialMode.cs
+++ Assets/Scripts/TutorialMode.cs
@@ -1,28 +1,23 @@
 ﻿/************************************************************************/
 /* Author:             Jaben Calas                                      */
 /* Date Created:       02/12/20                                         */
-/* Last Modified Date: 03/03/20                                         */
+/* Last Modified Date: 03/08/20                                         */
 /* Modified By:        J. Calas                                         */
 /************************************************************************/
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using System.Collections.Generic;
-using Photon.Pun;
-using System.Collections;
 using UnityEngine.SceneManagement;
+using TMPro;
+using Photon.Pun;
 
 public class TutorialMode : MonoBehaviour
 {
     #region Symbolic Constants
-    public const int step1 = 10;
-    public const int step2 = 20;
-    public const int step3 = 30;
-    public const int step4 = 40;
-    public const int step5 = 50;
-    public const int step6 = 60;
-    public const int step7 = 70;
-    public const int step8 = 80;
+    public const int step1 = 00;
+    public const int step2 = 05;
+    public const int step3 = 10;
+    public const int step4 = 15;
+    public const int step5 = 60;
+    public const int step6 = 90;
     #endregion
 
     #region Variables
@@ -45,18 +40,13 @@ public class TutorialMode : MonoBehaviour
 
     private Player          player;
     private int             lastStep;
-    private float           delayTime = 5.0f;
-    private bool            firstCall      = true;
+    private bool            firstCall;
     private string          sceneName;
     #endregion
 
-    #region Game Timer
-    public static double matchTimer = 0;
-    private int minute;
-    private int second;
+    #region GameTimer
     private double startTime;
-    private double matchLength = 80;
-    private double timer;
+    private int    gameTimer;
     #endregion
 
     void Awake()
@@ -74,37 +64,39 @@ public class TutorialMode : MonoBehaviour
 
         #region Initialize variables
         player          = GameObject.FindGameObjectWithTag("PlayerClass").GetComponent<Player>();
-        wall            = GameObject.Find("FirstWall");
-        wall2           = GameObject.Find("SecondWall");
+        wall            = GameObject.Find("Checkpoint (1)");
+        wall2           = GameObject.Find("Checkpoint (2)");
         panel           = GameObject.Find("Panel");
         block           = GameObject.Find("Block");
                         
         currentStep     = step1;
-        lastStep        = step8;
+        lastStep        = step6;
 
-        tutorialModeOn = false;
+        firstCall       = true;
+
+        tutorialModeOn  = false;
 
         promptText.gameObject.SetActive(false);
         promptText.text = string.Format("Press {0} to continue.", KeyCode.Space.ToString().ToLower());
-        #endregion
 
-        SetTimer();
-        matchTimer = 0;
+        gameTimer       = 0;
+        startTime       = PhotonNetwork.Time;
+        #endregion
     }
 
     void FixedUpdate()
     {
-        UpdateTimer();
+        #region Handles GameTimer
+        gameTimer = (int)(PhotonNetwork.Time - startTime);
 
-        Debug.Log(timer);
+        currentStep = gameTimer;
+        #endregion
 
         #region Moves tutorial to the next slide on GameUI
         if (sceneName == "Tutorial")
         {
             TutorialUIText();
         }
-
-        currentStep = (int)matchLength;
         #endregion
     }
 
@@ -146,28 +138,14 @@ public class TutorialMode : MonoBehaviour
 
             #region Step 5
             case step5:
-                headingText.text  = "5";
-                subtitleText.text = "5";
-                break;
-            #endregion
-
-            #region Step 6
-            case step6:
                 wall2.LeanMoveLocalY(-5, 1.5f);
                 headingText.text  = "Aim at the [target] and {click} to fire.";
                 subtitleText.text = "";
                 break;
             #endregion
 
-            #region Step 7
-            case step7:
-                headingText.text  = "7";
-                subtitleText.text = "7";
-                break;
-            #endregion
-
-            #region Step 8
-            case step8:
+            #region Step 6
+            case step6:
                 headingText.text  = "CONGRATULATIONS!";
                 subtitleText.text = "You have successfully completed the tutorial.";
                 break;
@@ -181,44 +159,5 @@ public class TutorialMode : MonoBehaviour
             firstCall = false;
         }
         #endregion
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        currentStep = 4;
-    }
-
-    IEnumerator NextStep()
-    {
-        yield return new WaitForSeconds(delayTime);
-        currentStep = 2;
-    }
-
-    public void SetTimer()
-    {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            startTime = PhotonNetwork.Time;
-            PhotonNetwork.CurrentRoom.SetCustomProperties(new ExitGames.Client.Photon.Hashtable() { { "StartTime", startTime } });
-        }
-        else
-        {
-            try
-            {
-                startTime = (double)PhotonNetwork.CurrentRoom.CustomProperties["StartTime"];
-            }
-            catch
-            {
-                startTime = PhotonNetwork.Time;
-            }
-        }
-    }
-
-    public void UpdateTimer()
-    {
-        matchTimer = PhotonNetwork.Time - startTime;
-        timer = matchLength + matchTimer;
-        second = (int)(timer % 60.0f);
-        minute = (int)(timer / 60.0f);
     }
 }
---
+++ Assets/Scripts/TutorialModeTriggers.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class TutorialModeTriggers : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        TutorialMode.currentStep = 5;
+    }
+}
