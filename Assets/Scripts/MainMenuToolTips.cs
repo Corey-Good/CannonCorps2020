aa2348398e57a3001b6fc7a25e268f2c7c1a2d93
JabenCalas <55771813+JabenCalas@users.noreply.github.com>
2020-03-11 14:35:49 -0500

    Updates to MainMenu: InfoMenu

--- Assets/Scripts/MainMenuToolTips.cs
+++ Assets/Scripts/MainMenuToolTips.cs
@@ -10,36 +10,49 @@ public class MainMenuToolTips : MonoBehaviour
 {
     public TextMeshProUGUI ToolTipsText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-
-    public void FFATips()
+    #region PlayMenu
+    public void PlayMenuFFATips()
     {
         ToolTipsText.text = "Try to get as many points as possible by eliminating opponents!";
     }
-    public void SMTips()
+    public void PlayMenuSMTips()
     {
        ToolTipsText.text =     "All players start off as minnows, with 1 player randomly becoming a shark." +
                            "\n\nThe shark will try to overpower as many minnows as possible, while minnows try to survive " +
                                "for as long as possible!";
     }
-    public void TBTips()
+    public void PlayMenuTBTips()
     {
        ToolTipsText.text =     "Defeat players on the enemy team to earn points for your team. " +
                            "\n\nThe team that reaches the score limit, or has more points when time expires wins!";
     }
-    public void TTTips()
+    public void PlayMenuTTTips()
     {
         ToolTipsText.text = "Click here to learn the basics!";
     }
+    #endregion
+
+    #region InfoMenu
+    public void InfoMenuFFATips()
+    {
+        ToolTipsText.text = "Free For All, or FFA, is a Game Mode in"+" which players attempt to accumulate the most amount of points with no team for support; instead, they must rely on themselves. Points are scored by hitting or eliminating other players. 10 Points are awarded for each hit.";
+
+
+    }
+    public void InfoMenuSMTips()
+    {
+        ToolTipsText.text = "All players start off as minnows, with 1 player randomly becoming a shark." +
+                            "\n\nThe shark will try to overpower as many minnows as possible, while minnows try to survive " +
+                                "for as long as possible!";
+    }
+    public void InfoMenuTBTips()
+    {
+        ToolTipsText.text = "Defeat players on the enemy team to earn points for your team. " +
+                            "\n\nThe team that reaches the score limit, or has more points when time expires wins!";
+    }
+    public void InfoMenuTTTips()
+    {
+        ToolTipsText.text = "Click here to learn the basics!";
+    }
+    #endregion
 }
