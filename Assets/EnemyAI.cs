4ed39f6422a63ddf51df234a561e9b9e1bee2740
Corey-Good <55670356+Corey-Good@users.noreply.github.com>
2020-04-11 15:33:13 -0500

    Testing something

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject target;
+    private float speed = 10f;
+    private float stoppingDistance = 15f;
+    private float retreatDistance = 10f;
+    Quaternion direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Quaternion.LookRotation(target.transform.position - transform.position);
+
+        if (Vector3.Distance(transform.position, target.transform.position) > stoppingDistance)
+        {
+            transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, direction, speed * 20 * Time.deltaTime);
+            Debug.Log("Follow");
+        }
+        else if (Vector3.Distance(transform.position, target.transform.position) < stoppingDistance && Vector3.Distance(transform.position, target.transform.position) > retreatDistance)
+        {
+            // do nothing
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, direction, speed * 40 * Time.deltaTime);
+            Debug.Log("Stay");
+        }
+        else if (Vector3.Distance(transform.position, target.transform.position) < retreatDistance)
+        {
+            transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, -speed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, direction, -speed * 20 * Time.deltaTime);
+            Debug.Log("Retreat");
+        }
+    }
+}
